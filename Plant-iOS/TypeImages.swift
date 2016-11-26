212b739ea60ed4293eddc299b329e8bb6bb7af29
Li Maoqi <lmq729981607@gmail.com>
2016-11-26 23:01:31 +0800

    message

---
+++ Plant-iOS/TypeImages.swift
@@ -0,0 +1,14 @@
+//
+//  TypeImages.swift
+//  Plant-iOS
+//
+//  Created by 李茂琦 on 26/11/2016.
+//  Copyright © 2016 Li Maoqi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+var TypeImages = [
+    "flower": UIImage(named: "flower")
+]
